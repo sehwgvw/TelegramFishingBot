2b22bf02636edc0a06a18a67b5452e1cb2ccdd74
sehwgvw <kritedi@gmail.com>
2025-11-18 15:56:07 +0300

    Create web_server.py

---
+++ web_server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_web():
+    server = Thread(target=run)
+    server.daemon = True
+    server.start()
