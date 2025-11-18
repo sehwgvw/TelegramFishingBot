15f9423b4241a913d78ac05193a89da3648c420f
sehwgvw <kritedi@gmail.com>
2025-11-18 16:25:45 +0300

    Update ping_server.py

--- ping_server.py
+++ ping_server.py
@@ -1,15 +1,9 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🤖 Bot is ALIVE and RUNNING 24/7!"
-
-@app.route('/health')
-def health():
-    return "OK"
+    return "PING"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
