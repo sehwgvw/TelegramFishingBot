b18a1261a1936651e13acfe723b06c94367cbb64
sehwgvw <kritedi@gmail.com>
2025-11-18 15:28:10 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# === НАСТРОЙКИ БОТА ===
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8538101699:AAE_zPwy8Zgl8BSJcceWvSFooSsPGyJvZ3U")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7544069555"))
+
+# === API CREDENTIALS ===
+API_CREDENTIALS = [
+    {
+        "api_id": int(os.getenv("API_ID", "32114806")),
+        "api_hash": os.getenv("API_HASH", "16ece23e5b4033a9aa9a307d1d508be4")
+    }
+]
+
+# === РЕФЕРАЛЬНАЯ СИСТЕМА ===
+REFERRAL_BONUS = 50
